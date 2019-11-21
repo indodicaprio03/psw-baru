449731e0c7c755e63368d53021ba2a6e6eb99dd2
indodicaprio03 <indo.dicaprio03@gmail.com>
2019-11-21 15:00:58 +0700

    new

---
+++ psw-project-master(1)/psw-project-master/PSW/js/javaScript.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    var previous = document.getElementById("menu8"); //The last opened menu, "menu8" by default since it is opened on page load
+	var original = "menu"; //Concatenate this with the element id
+	var toOpen = ""; //Store the concatenated value
+	var over; //Boolean: is the move over the element or not?
+
+	var delay = function (elem, callback) { //Delay the execution of the "callback" argument on the "elem" argument
+		var timeout = null; //Store the timer
+		timeout = setTimeout(function() { //Set a timer
+			if(over) { //If the mouse is still over the element, do:
+				callback(elem); //Call the function that was passed as an argument
+			}
+		}, 150); //Time in milliseconds before the timer triggers
+
+		$(elem).mouseout(function() { //If the mouse leaves the element, do:
+			over = false; //Set the boolean to false
+			clearTimeout(timeout); //Clear the timer
+		});
+	};
+
+	$(".over").mouseenter(function(event) { //When the mouse enters one of the menu's elements, do:
+		over = true; //Set the boolean to true
+		delay(document.getElementById(event.target.id), function(context) { //Call the timer function
+			toOpen = document.getElementById(original.concat(context.id)); //When the timer is done, get the menu to open
+			openMenu(toOpen); //Open it
+			previous = toOpen; //Set it as the "previous" menu for the next instance of this function
+		});
+	});
+
+	$(".clicky").click(function() {
+		openMenu(document.getElementById("menu8"));
+		previous = document.getElementById("menu8");
+	});
+
+	function openMenu(context) { //Open the specified menu
+		closeMenu(previous);
+		$(context).toggleClass("visible"); //Toggle the visibility of the element
+		$(context).toggleClass("hidden");
+	}
+
+	function closeMenu(context) { //Close the specified menu
+		$(context).toggleClass("visible"); //Toggle the visibility of the element
+		$(context).toggleClass("hidden");
+	}
+});
+
+
+var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
